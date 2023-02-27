5facf08b9f891925fa150a46f2c1548471308b30
Pierre657 <62481379+Pierre657@users.noreply.github.com>
2023-02-27 22:04:14 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const text = document.querySelector('.text')
+const img = document.querySelector('.image')
+
+document.addEventListener("DOMContentLoaded", function() {
+     setTimeout(() => {
+         text.classList.add('vis');
+         setTimeout(() => {
+            text.style.color = 'pink'
+            img.style.backgroundImage = 'url(/beach-1852945_1280.jpg)';
+         }, 300);
+    }, 100);
+    
+   
+})
