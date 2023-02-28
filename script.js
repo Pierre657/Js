706bcda37b3ed95317e34aa8d20a634a6e55d101
Pierre657 <62481379+Pierre657@users.noreply.github.com>
2023-02-28 08:37:34 +0100

    ee

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const text = document.querySelector('.text')
 const img = document.querySelector('.image')
+const ici = document.querySelector('.ici')
 
 document.addEventListener("DOMContentLoaded", function() {
      setTimeout(() => {
          text.classList.add('vis');
-         setTimeout(() => {
-            text.style.color = 'pink'
-            // img.style.backgroundImage = 'url(/beach-1852945_1280.jpg)';
-         }, 1300);
-    }, 100);
+        //  setTimeout(() => {
+        //     text.style.color = 'pink'
+        //     ici.style.border = '10px solid white'
+        //     ici.style.borderRadius = '30px'
+
+        //     // img.style.backgroundImage = 'url(/beach-1852945_1280.jpg)';
+        //  }, 1000);
+    }, 0.1);
     
    
 })
